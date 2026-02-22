075975c8ba166a24ec27a08fd97c869015d12c3a
saurabhKl16 <saurabhlom1998@gmail.com>
2026-02-22 18:00:49 +0530

    Create - Login and Signup page

---
+++ login.js
@@ -0,0 +1,7 @@
+console.log("welcome");
+
+const loginBtn = document.getElementById("LoginButton");
+
+loginBtn.addEventListener("click", () => {
+  window.location.href = "pages/home-page.html";
+});
---
+++ pages/signup/signup.js
@@ -0,0 +1,10 @@
+const signupBtn = document.getElementById("SignupButton");
+const loginBtn = document.getElementById("LoginButton");
+
+
+signupBtn.addEventListener("click", ()=>{
+    window.location.href = "../../index.html"
+})
+loginBtn.addEventListener("click", ()=>{
+    window.location.href = "../../index.html"
+})
