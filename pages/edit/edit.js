7a38c0adeaf148124d7a3cd4ad9aa15e4fc32e75
saurabhKl16 <saurabhlom1998@gmail.com>
2026-02-23 10:29:42 +0530

    Create : edit employee

---
+++ pages/edit/edit.js
@@ -0,0 +1,14 @@
+const editForm = document.getElementById("editForm");
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const employee = {
+    name: document.getElementById("name").value,
+    id: document.getElementById("employeeId").value,
+    position: document.getElementById("employeePosition").value,
+    email: document.getElementById("employeeEmail").value,
+    mobile: document.getElementById("employeeMobile").value,
+  }
+  console.log(employee);
+});
